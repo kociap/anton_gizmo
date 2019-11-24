49f79b53ffcaccb5e7ef0b7c0689a9bd6c3067bc
Piotr Kocia <p.kocia@wp.pl>
2019-11-24 19:09:10 +0100

    docs and examples

--- include/anton_gizmo/dial_3d.hpp
+++ include/anton_gizmo/dial_3d.hpp
@@ -16,6 +16,12 @@ namespace anton {
     // Returns how many elements a buffer should have at least to fit a gizmo with given number of vertices.
     uint32_t get_required_buffer_size_dial_3d(uint32_t vertex_count);
 
+    // Writes vertex positions, axes of rotations to align the line with the view and scale factors to preserve the width at the corners.
+    // The circle lies on the plane z = 0.
+    // The function writes 2 vertices per line vertex to later expand them into a wide line in a shader
+    //   (example shader may be found in examples/).
+    //
+    // Returns the number of vertices written.
     uint32_t generate_dial_3d_geometry(uint32_t vertex_count, Vector3* vertices, Vector3* rotation_axes, float* scale_factors);
 
     // Tests the arrow for intersection and returns the distance to the intersection point along the ray if the ray intersects the bounding volumes.
