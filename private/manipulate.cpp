91392132c0f965f41891f6d9deb9d5111637365d
Piotr Kocia <p.kocia@wp.pl>
2020-08-02 12:33:15 +0200

    fixed translate_along_line

--- private/manipulate.cpp
+++ private/manipulate.cpp
@@ -4,16 +4,16 @@
 
 namespace anton::gizmo {
     math::Vector3 translate_along_line(Ray const ray, math::Vector3 const axis, math::Vector3 const origin) {
-        math::Vector3 const point_on_line = ray.origin - origin - axis * math::dot(ray.origin - origin, axis);
-        math::Vector3 const plane_normal = math::normalize(ray.origin - point_on_line);
-        f32 const plane_distance = math::dot(point_on_line, plane_normal);
+        math::Vector3 const point_on_axis = origin + axis * math::dot(ray.origin - origin, axis);
+        math::Vector3 const plane_normal = math::normalize(ray.origin - point_on_axis);
+        f32 const plane_distance = math::dot(origin, plane_normal);
         auto res = intersect_ray_plane(ray, plane_normal, plane_distance);
         if(!res) {
             return origin;
         }
 
         math::Vector3 const& hit_point = res.value().hit_point;
-        math::Vector3 position = math::dot(hit_point - origin, axis) * axis;
+        math::Vector3 position = origin + math::dot(hit_point - origin, axis) * axis;
         return position;
     }
 
