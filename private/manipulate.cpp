d4ef3bd17756f0593f1055f2621191e173f81fc4
Piotr Kocia <p.kocia@wp.pl>
2020-08-04 12:22:37 +0200

    initial ray for cursor offset

--- private/manipulate.cpp
+++ private/manipulate.cpp
@@ -3,28 +3,35 @@
 #include <intersection_tests.hpp>
 
 namespace anton::gizmo {
-    math::Vector3 translate_along_line(Ray const ray, math::Vector3 const axis, math::Vector3 const origin) {
+    math::Vector3 translate_along_line(Ray const ray, math::Vector3 const axis, math::Vector3 const origin, Ray const initial_ray) {
         math::Vector3 const point_on_axis = origin + axis * math::dot(ray.origin - origin, axis);
         math::Vector3 const plane_normal = math::normalize(ray.origin - point_on_axis);
         f32 const plane_distance = math::dot(origin, plane_normal);
-        auto res = intersect_ray_plane(ray, plane_normal, plane_distance);
-        if(!res) {
-            return origin;
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
         }
 
-        math::Vector3 const& hit_point = res.value().hit_point;
-        math::Vector3 position = origin + math::dot(hit_point - origin, axis) * axis;
-        return position;
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return origin + math::dot(res->hit_point - offset, axis) * axis;
+        } else {
+            return origin;
+        }
     }
 
-    math::Vector3 translate_along_plane(Ray const ray, math::Vector3 const plane_normal, math::Vector3 const origin) {
+    math::Vector3 translate_along_plane(Ray const ray, math::Vector3 const plane_normal, math::Vector3 const origin, Ray const initial_ray) {
         f32 const plane_distance = math::dot(origin, plane_normal);
-        auto res = intersect_ray_plane(ray, plane_normal, plane_distance);
-        if(!res) {
-            return origin;
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
         }
 
-        math::Vector3 const& hit_point = res.value().hit_point;
-        return hit_point;
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return origin + res->hit_point - offset;
+        } else {
+            return origin;
+        }
     }
 } // namespace anton::gizmo
--- public/anton/gizmo/manipulate.hpp
+++ public/anton/gizmo/manipulate.hpp
@@ -9,7 +9,7 @@ namespace anton::gizmo {
     // Returns:
     // The translated position
     //
-    [[nodiscard]] math::Vector3 translate_along_line(Ray ray, math::Vector3 axis, math::Vector3 origin);
+    [[nodiscard]] math::Vector3 translate_along_line(Ray ray, math::Vector3 axis, math::Vector3 origin, Ray initial_ray);
 
     // translate_along_plane
     // origin - the original position of the object
@@ -17,5 +17,5 @@ namespace anton::gizmo {
     // Returns:
     // The translated position
     //
-    [[nodiscard]] math::Vector3 translate_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin);
+    [[nodiscard]] math::Vector3 translate_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin, Ray initial_ray);
 } // namespace anton::gizmo
