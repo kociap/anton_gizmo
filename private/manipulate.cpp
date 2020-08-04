88359871a549da2a39d7ff150e103a8a1cc6221a
Piotr Kocia <p.kocia@wp.pl>
2020-08-04 15:20:59 +0200

    scale manipulation functions

--- private/manipulate.cpp
+++ private/manipulate.cpp
@@ -34,4 +34,72 @@ namespace anton::gizmo {
             return origin;
         }
     }
+
+    math::Vector3 scale_along_line(Ray const ray, math::Vector3 const axis, math::Vector3 const origin, Ray const initial_ray,
+                                   math::Vector3 const initial_scale) {
+        math::Vector3 const point_on_axis = origin + axis * math::dot(ray.origin - origin, axis);
+        math::Vector3 const plane_normal = math::normalize(ray.origin - point_on_axis);
+        f32 const plane_distance = math::dot(origin, plane_normal);
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
+        }
+
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return initial_scale + math::dot(res->hit_point - offset, axis) * axis;
+        } else {
+            return initial_scale;
+        }
+    }
+
+    math::Vector3 scale_along_plane(Ray const ray, math::Vector3 const plane_normal, math::Vector3 const origin, Ray const initial_ray,
+                                    math::Vector3 const initial_scale) {
+        f32 const plane_distance = math::dot(origin, plane_normal);
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
+        }
+
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return initial_scale + res->hit_point - offset;
+        } else {
+            return initial_scale;
+        }
+    }
+
+    math::Vector3 scale_uniform_along_line(Ray const ray, math::Vector3 const axis, math::Vector3 const origin, Ray const initial_ray,
+                                           math::Vector3 const initial_scale) {
+        math::Vector3 const point_on_axis = origin + axis * math::dot(ray.origin - origin, axis);
+        math::Vector3 const plane_normal = math::normalize(ray.origin - point_on_axis);
+        f32 const plane_distance = math::dot(origin, plane_normal);
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
+        }
+
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return initial_scale + math::dot(res->hit_point - offset, axis);
+        } else {
+            return initial_scale;
+        }
+    }
+
+    math::Vector3 scale_uniform_along_plane(Ray const ray, math::Vector3 const plane_normal, math::Vector3 const origin, Ray const initial_ray,
+                                            math::Vector3 const initial_scale) {
+        f32 const plane_distance = math::dot(origin, plane_normal);
+        // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
+        math::Vector3 offset;
+        if(auto res = intersect_ray_plane(initial_ray, plane_normal, plane_distance)) {
+            offset = res->hit_point;
+        }
+
+        if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
+            return initial_scale + math::length(res->hit_point - offset);
+        } else {
+            return initial_scale;
+        }
+    }
 } // namespace anton::gizmo
--- public/anton/gizmo/manipulate.hpp
+++ public/anton/gizmo/manipulate.hpp
@@ -17,5 +17,38 @@ namespace anton::gizmo {
     // Returns:
     // The translated position
     //
-    [[nodiscard]] math::Vector3 translate_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin, Ray initial_ray);
+    [[nodiscard]] math::Vector3 translate_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin, Ray initial_ray, math::Vector3 initial_scale);
+
+    // scale_along_line
+    // origin - the original position of the object
+    //
+    // Returns:
+    // The transformed scale
+    //
+    [[nodiscard]] math::Vector3 scale_along_line(Ray ray, math::Vector3 axis, math::Vector3 origin, Ray initial_ray, math::Vector3 initial_scale);
+
+    // scale_along_plane
+    // origin - the original position of the object
+    //
+    // Returns:
+    // The transformed scale
+    //
+    [[nodiscard]] math::Vector3 scale_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin, Ray initial_ray, math::Vector3 initial_scale);
+
+    // scale_uniform_along_line
+    // origin - the original position of the object
+    //
+    // Returns:
+    // The transformed scale
+    //
+    [[nodiscard]] math::Vector3 scale_uniform_along_line(Ray ray, math::Vector3 axis, math::Vector3 origin, Ray initial_ray, math::Vector3 initial_scale);
+
+    // scale_uniform_along_plane
+    // origin - the original position of the object
+    //
+    // Returns:
+    // The transformed scale
+    //
+    [[nodiscard]] math::Vector3 scale_uniform_along_plane(Ray ray, math::Vector3 plane_normal, math::Vector3 origin, Ray initial_ray,
+                                                          math::Vector3 initial_scale);
 } // namespace anton::gizmo
