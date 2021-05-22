382933affb0d0b536aee35f2491e2cf02448cc56
Piotr Kocia <p.kocia@wp.pl>
2021-05-22 16:48:34 +0200

    fixed scale manipulation to work in any space

--- private/manipulate.cpp
+++ private/manipulate.cpp
@@ -32,7 +32,8 @@ namespace anton::gizmo {
 
     math::Vec3 translate_along_plane(math::Mat4 const inverse_parent_transform, math::Ray const ray, math::Vec3 const first_axis, math::Vec3 const second_axis,
                                      math::Vec3 const origin, math::Ray const initial_ray, math::Vec3 const initial_position, f32 const snap) {
-        math::Vec3 const plane_normal = math::cross(first_axis, second_axis);
+        // The axes are NOT necessarily perpendicular
+        math::Vec3 const plane_normal = math::normalize(math::cross(first_axis, second_axis));
         f32 const plane_distance = math::dot(origin, plane_normal);
         // Calculate cursor offset that we'll use to prevent the center of the object from snapping to the cursor
         auto const initial_res = intersect_ray_plane(initial_ray, plane_normal, plane_distance);
@@ -57,9 +58,9 @@ namespace anton::gizmo {
         }
     }
 
-    math::Vec3 scale_along_line(math::Ray const ray, math::Vec3 const axis, math::Vec3 const origin, math::Ray const initial_ray,
-                                math::Vec3 const initial_scale, f32 const speed, f32 const snap) {
-        math::Vec3 const point_on_axis = origin + axis * math::dot(ray.origin - origin, axis);
+    math::Vec3 scale_along_line(math::Mat4 const inverse_parent_transform, math::Ray const ray, math::Vec3 const axis_world, math::Vec3 const axis_local,
+                                math::Vec3 const origin, math::Ray const initial_ray, math::Vec3 const initial_scale, f32 const snap) {
+        math::Vec3 const point_on_axis = origin + axis_world * math::dot(ray.origin - origin, axis_world);
         math::Vec3 const plane_normal = math::normalize(ray.origin - point_on_axis);
         f32 const plane_distance = math::dot(origin, plane_normal);
         auto const initial_res = intersect_ray_plane(initial_ray, plane_normal, plane_distance);
@@ -67,18 +68,16 @@ namespace anton::gizmo {
             return initial_scale;
         }
 
-        math::Vec3 const offset = initial_res->hit_point;
         if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
-            math::Vec3 const hit = res->hit_point;
-            f32 const offset_line_length = math::dot(offset - origin, axis);
-            f32 const hit_line_length = math::dot(hit - origin, axis);
+            f32 const offset_line_length = math::dot(initial_res->hit_point - origin, axis_world);
+            f32 const hit_line_length = math::dot(res->hit_point - origin, axis_world);
             f32 factor = hit_line_length / offset_line_length;
-            factor *= speed;
             if(snap != 0.0f) {
                 factor = math::round_to_nearest(factor, snap);
             }
+
             // Find out how much scale is along axis
-            math::Vec3 const scale_along_axis = math::dot(initial_scale, axis) * axis;
+            math::Vec3 const scale_along_axis = math::dot(initial_scale, axis_local) * axis_local;
             // The rest of the scale should not be changed
             math::Vec3 const remaining_scale = initial_scale - scale_along_axis;
             return factor * scale_along_axis + remaining_scale;
@@ -87,36 +86,34 @@ namespace anton::gizmo {
         }
     }
 
-    math::Vec3 scale_along_plane(math::Ray const ray, math::Vec3 const plane_normal, math::Vec3 const origin, math::Ray const initial_ray,
-                                 math::Vec3 const initial_scale, f32 const speed, f32 const snap) {
+    math::Vec3 scale_along_plane(math::Mat4 const inverse_parent_transform, math::Ray const ray, math::Vec3 const first_axis_world,
+                                 math::Vec3 const first_axis_local, math::Vec3 const second_axis_world, math::Vec3 const second_axis_local,
+                                 math::Vec3 const origin, math::Ray const initial_ray, math::Vec3 const initial_scale, f32 const snap) {
+        // The axes are NOT necessarily perpendicular
+        math::Vec3 const plane_normal = math::normalize(math::cross(first_axis_world, second_axis_world));
         f32 const plane_distance = math::dot(origin, plane_normal);
         auto const initial_res = intersect_ray_plane(initial_ray, plane_normal, plane_distance);
         if(!initial_res) {
             return initial_scale;
         }
 
-        math::Vec3 const offset = initial_res->hit_point;
         if(auto res = intersect_ray_plane(ray, plane_normal, plane_distance)) {
-            math::Vec3 const hit = res->hit_point;
-            math::Vec3 const origin_offset = offset - origin;
-            math::Vec3 const origin_hit = hit - origin;
+            math::Vec3 const origin_offset = initial_res->hit_point - origin;
+            math::Vec3 const origin_hit = res->hit_point - origin;
             f32 const origin_offset_length = math::length(origin_offset);
             f32 const origin_hit_length = math::length(origin_hit);
             f32 const sign = math::dot(origin_offset, origin_hit) >= 0 ? 1 : -1;
             f32 factor = sign * origin_hit_length / origin_offset_length;
-            factor *= speed;
             if(snap != 0.0f) {
                 factor = math::round_to_nearest(factor, snap);
             }
-            // Find 2 random vectors in the plane
-            math::Vec3 const v1 = math::perpendicular(plane_normal);
-            math::Vec3 const v2 = math::cross(v1, plane_normal);
-            // Find out how much scale is along those vectors
-            math::Vec3 const scale_along_v1 = math::dot(initial_scale, v1) * v1;
-            math::Vec3 const scale_along_v2 = math::dot(initial_scale, v2) * v2;
+            // Find out how much scale is not in the plane defined
+            // by the local axes and should not be modified.
+            math::Vec3 const plane_normal_local = math::normalize(math::cross(first_axis_local, second_axis_local));
+            math::Vec3 const remaining_scale = math::dot(initial_scale, plane_normal_local) * plane_normal_local;
             // The rest of the scale should not be changed
-            math::Vec3 const remaining_scale = initial_scale - scale_along_v1 - scale_along_v2;
-            return factor * scale_along_v1 + factor * scale_along_v2 + remaining_scale;
+            math::Vec3 const scale_in_plane = initial_scale - remaining_scale;
+            return factor * scale_in_plane + remaining_scale;
         } else {
             return initial_scale;
         }
--- public/anton/gizmo/manipulate.hpp
+++ public/anton/gizmo/manipulate.hpp
@@ -15,38 +15,38 @@ namespace anton::gizmo {
     // inverse_parent_transform - transform from the world space to the parent space of the object.
     //                            For manipulation of unparented objects use the identity matrix.
     //                      ray - the current ray in the world space constructed by unprojecting the cursor.
-    //                     axis - the axis in the world space along which change from initial_ray will affect the scale. Must be normalized.
+    //                     axis - the axis in the world space along which change from initial_ray will affect the position. Must be normalized.
     //                   origin - the origin of the manipulation in the world space used to determine the position of the line.
-    //              initial_ray - the ray at the start of the manipulation.
-    //         initial_position - the position at the start of the manipulation in the local space of the object.
+    //              initial_ray - the ray in the world space at the start of the manipulation in the world space.
+    //         initial_position - the position at the start of the manipulation in the parent space of the object.
     //                     snap - grid snapping (disabled if snap == 0). Change in position will be rounded to the nearest multiple of snap.
     //
     // Returns:
-    // The translated position in the parent space.
+    // The changed position in the parent space.
     //
     [[nodiscard]] math::Vec3 translate_along_line(math::Mat4 inverse_parent_transform, math::Ray ray, math::Vec3 axis, math::Vec3 origin, math::Ray initial_ray,
                                                   math::Vec3 initial_position, f32 snap = 0.0f);
 
     // translate_along_plane
-    // Translate in the directions perpendicular to plane_normal.
+    // Translate in the plane spanned by first_axis and second_axis.
     // The position change is calculated based on the difference between initial_ray
-    // and ray in the plane defined by plane_normal and origin.
+    // and ray in the plane defined by first_axis, second_axis and origin.
     //
     // Parameters:
     // inverse_parent_transform - transform from the world space to the parent space of the object.
     //                            For manipulation of unparented objects use the identity matrix.
     //                      ray - the current ray in the world space constructed by unprojecting the cursor.
-    //               first_axis - first axis defining the plane in the world space along which change from initial_ray will affect the scale.
-    //                            Must be nornalized.
-    //              second_axis - second axis defining the plane in the world space along which change from initial_ray will affect the scale.
-    //                            Must be nornalized.
-    //                   origin - the origin of the manipulation in the world space used to determine the position of the line.
+    //               first_axis - first axis defining the plane in the world space along which change from initial_ray will affect the position.
+    //                            Must be nornalized. Must not be collinear with second_axis.
+    //              second_axis - second axis defining the plane in the world space along which change from initial_ray will affect the position.
+    //                            Must be nornalized. Must not be collinear with first_axis.
+    //                   origin - the origin of the manipulation in the world space used to determine the position of the plane.
     //              initial_ray - the ray in the world space at the start of the manipulation in the world space.
-    //         initial_position - the position at the start of the manipulation in the local space of the object.
+    //         initial_position - the position at the start of the manipulation in the parent space of the object.
     //                     snap - grid snapping (disabled if snap == 0). Change in position will be rounded to the nearest multiple of snap.
     //
     // Returns:
-    // The translated position in the parent space.
+    // The changed position in the parent space.
     //
     [[nodiscard]] math::Vec3 translate_along_plane(math::Mat4 inverse_parent_transform, math::Ray ray, math::Vec3 first_axis, math::Vec3 second_axis,
                                                    math::Vec3 origin, math::Ray initial_ray, math::Vec3 initial_position, f32 snap = 0.0f);
@@ -56,39 +56,48 @@ namespace anton::gizmo {
     // the difference between initial_ray and ray along axis.
     //
     // Parameters:
-    // ray           - the current ray constructed by unprojecting the cursor.
-    // axis          - the axis along which change from initial_ray will affect the scale. Must be nornalized.
-    // origin        - the origin of the manipulation used to determine the position of the line.
-    // initial_ray   - the ray at the start of the manipulation.
-    // initial_scale - the scale at the start of the manipulation.
-    // speed         - a factor to multiply the calculated change by.
-    // snap          - grid snapping (disabled if snap == 0). Change in scale will be rounded to the nearest multiple of snap.
+    // inverse_parent_transform - transform from the world space to the parent space of the object.
+    //                            For manipulation of unparented objects use the identity matrix.
+    //                      ray - the current ray in the world space constructed by unprojecting the cursor.
+    //               axis_world - the axis in the world space along which change from initial_ray will affect the scale. Must be normalized.
+    //               axis_local - the axis in the local space along which change from initial_ray will affect the scale. Must be normalized.
+    //                   origin - the origin of the manipulation in the world space used to determine the position of the line.
+    //              initial_ray - the ray in the world space at the start of the manipulation in the world space.
+    //         initial_position - the position at the start of the manipulation in the parent space of the object.
+    //                     snap - grid snapping (disabled if snap == 0). Change in position will be rounded to the nearest multiple of snap.
     //
     // Returns:
-    // The transformed scale.
+    // The changed scale in the local space.
     //
-    [[nodiscard]] math::Vec3 scale_along_line(math::Ray ray, math::Vec3 axis, math::Vec3 origin, math::Ray initial_ray, math::Vec3 initial_scale, f32 speed,
-                                              f32 snap = 0.0f);
+    [[nodiscard]] math::Vec3 scale_along_line(math::Mat4 inverse_parent_transform, math::Ray ray, math::Vec3 axis_world, math::Vec3 axis_local,
+                                              math::Vec3 origin, math::Ray initial_ray, math::Vec3 initial_scale, f32 snap = 0.0f);
 
     // scale_along_plane
-    // Scale uniformly in the directions perpendicular to plane_normal.
+    // Scale uniformly in the plane spanned by first_axis and second_axis.
     // The scale change is calculated based on the difference between initial_ray
-    // and ray in the plane defined by plane_normal and origin.
+    // and ray in the plane defined by first_axis, second_axis and origin.
     //
     // Parameters:
-    // ray           - the current ray constructed by unprojecting the cursor.
-    // plane_normal  - normal defining the manipulation plane. Must be normalized.
-    // origin        - the origin of the manipulation used to determine the position of the plane.
-    // initial_ray   - the ray at the start of the manipulation.
-    // initial_scale - the scale at the start of the manipulation.
-    // speed         - a factor to multiply the calculated change by.
-    // snap          - grid snapping (disabled if snap == 0). Change in scale will be rounded to the nearest multiple of snap.
+    // inverse_parent_transform - transform from the world space to the parent space of the object.
+    //                            For manipulation of unparented objects use the identity matrix.
+    //                      ray - the current ray in the world space constructed by unprojecting the cursor.
+    //         first_axis_world - first axis defining the plane in the world space along which change from initial_ray will affect the scale.
+    //                            Must be nornalized. Must not be collinear with second_axis_world.
+    //         first_axis_local - first axis in the local space of the object. Must be normalized. Must not be collinear with second_axis_local.
+    //        second_axis_world - second axis defining the plane in the world space along which change from initial_ray will affect the scale.
+    //                            Must be nornalized. Must not be collinear with first_axis_world.
+    //        second_axis_local - second axis in the local space of the object. Must be normalized. Must not be collinear with first_axis_local.
+    //                   origin - the origin of the manipulation in the world space used to determine the position of the plane.
+    //              initial_ray - the ray in the world space at the start of the manipulation in the world space.
+    //         initial_position - the position at the start of the manipulation in the parent space of the object.
+    //                     snap - grid snapping (disabled if snap == 0). Change in position will be rounded to the nearest multiple of snap.
     //
     // Returns:
-    // The transformed scale.
+    // The changed scale in the local space.
     //
-    [[nodiscard]] math::Vec3 scale_along_plane(math::Ray ray, math::Vec3 plane_normal, math::Vec3 origin, math::Ray initial_ray, math::Vec3 initial_scale,
-                                               f32 speed, f32 snap = 0.0f);
+    [[nodiscard]] math::Vec3 scale_along_plane(math::Mat4 inverse_parent_transform, math::Ray ray, math::Vec3 first_axis_world, math::Vec3 first_axis_local,
+                                               math::Vec3 second_axis_world, math::Vec3 second_axis_local, math::Vec3 origin, math::Ray initial_ray,
+                                               math::Vec3 initial_scale, f32 snap = 0.0f);
 
     // scale_uniform_along_line
     // Scale uniformly in all directions. The scale change is calculated based on
